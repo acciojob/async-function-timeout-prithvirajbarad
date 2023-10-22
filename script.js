3272f4eb4d8a8b54965068547f720ee263197717
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-22 19:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-//your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function () {
-  const textInput = document.getElementById('text');
-  const delayInput = document.getElementById('delay');
-  const outputDiv = document.getElementById('output');
-  const btn = document.getElementById('btn');
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
 
-  btn.addEventListener('click', async function () {
-    const text = textInput.value;
-    const delay = parseInt(delayInput.value);
-
-    if (!text || isNaN(delay)) {
-      outputDiv.innerText = 'Please enter valid text and delay.';
-      return;
-    }
-
-    outputDiv.innerText = 'Waiting...';
-
-    // Use a promise to simulate the delay
-    await new Promise((resolve) => setTimeout(resolve, delay));
-
-    // Display the message after the delay
-    outputDiv.innerText = text;
-  });
+//your code here
+async function showMessage() {
+  const message = text.value;
+  const delayVal = delay.value;
+  await new Promise((resolve) => setTimeout(resolve, delayVal));
+  output.innerText = message;
+}
+showMessage().then(()=>{
+	output.innerHTML = text.value;
 });
+btn.addEventListener("click", showMessage);
